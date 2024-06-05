f9ba1f44a3e338f08cdf10fcb5189744ad682f22
Mixail323 <mimi070904@mail.ru>
2024-06-05 17:47:59 +0300

    добавлена функция вывода возрата человека и её определение

--- Project3/PrintPars.cpp
+++ Project3/PrintPars.cpp
@@ -10,3 +10,8 @@ void printPersonFIO(Person p)
 {
 	cout << "װָ־:" << p.name << " " << p.secondname << " " << p.surname << "." << endl;
 }
+
+void printPersonAge(Person p)
+{
+	cout << "גמחנאסע: " << p.age << endl;
+}
